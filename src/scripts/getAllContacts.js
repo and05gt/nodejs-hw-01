891aa0f7eeed2d02cd822adbc27f947e8929ade0
Andrii D. <and05gt@gmail.com>
2024-11-07 23:08:17 +0100

    Add path and utils

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,11 @@
-export const getAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts';
+
+export const getAllContacts = async () => {
+  const contacts = await readContacts();
+
+  console.log(contacts);
+};
+
+getAllContacts().catch((error) => console.error(error));
 
 console.log(await getAllContacts());
